d3f1b823ba44c34dbc00fe07299945623e27ca15
Faan <rudfanno1@gmail.com>
2023-09-04 16:44:24 +0700

    add: mobile camera switch

--- src/components/Camera.jsx
+++ src/components/Camera.jsx
@@ -1,29 +1,52 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import Webcam from 'react-webcam';
 
 const CameraComponent = () => {
   const webcamRef = useRef(null);
   const [isCameraOn, setIsCameraOn] = useState(false);
+  const [cameraDevices, setCameraDevices] = useState([]);
+  const [selectedDeviceId, setSelectedDeviceId] = useState(null);
 
+  // Function to toggle camera on/off
   const toggleCamera = () => {
     setIsCameraOn((prevIsCameraOn) => !prevIsCameraOn);
   };
 
-  const capture = () => {
-    if (webcamRef.current) {
-      const imageSrc = webcamRef.current.getScreenshot();
-      // Do something with the captured image, e.g., send it to the server or display it.
-      console.log('Captured image:', imageSrc);
+  // Function to switch between available cameras
+  const switchCamera = () => {
+    if (cameraDevices.length > 1) {
+      const currentIndex = cameraDevices.findIndex((device) => device.deviceId === selectedDeviceId);
+      const nextIndex = (currentIndex + 1) % cameraDevices.length;
+      setSelectedDeviceId(cameraDevices[nextIndex].deviceId);
     }
   };
 
+  useEffect(() => {
+    // Get the list of available video devices when the component mounts
+    navigator.mediaDevices.enumerateDevices()
+      .then((devices) => {
+        const videoDevices = devices.filter((device) => device.kind === 'videoinput');
+        setCameraDevices(videoDevices);
+        // Set the default camera (usually the first one) as the selected camera
+        if (videoDevices.length > 0) {
+          setSelectedDeviceId(videoDevices[0].deviceId);
+        }
+      })
+      .catch((error) => {
+        console.error('Error enumerating devices:', error);
+      });
+  }, []);
+
   return (
     <div className="container mx-auto mt-8 text-center">
-      {isCameraOn && (
+      {selectedDeviceId && isCameraOn && (
         <Webcam
           audio={false}
           ref={webcamRef}
           screenshotFormat="image/jpeg"
+          videoConstraints={{
+            deviceId: selectedDeviceId,
+          }}
           className="w-full h-auto"
         />
       )}
@@ -35,12 +58,12 @@ const CameraComponent = () => {
       >
         {isCameraOn ? 'Turn Off Camera' : 'Turn On Camera'}
       </button>
-      {isCameraOn && (
+      {cameraDevices.length > 1 && (
         <button
-          onClick={capture}
+          onClick={switchCamera}
           className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-full hover:bg-blue-600"
         >
-          Capture Photo
+          Switch Camera
         </button>
       )}
     </div>
