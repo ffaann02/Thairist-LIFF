5422e1f0680f032a9c6e678209d2536cd07a406d
Faan <rudfanno1@gmail.com>
2024-01-17 00:35:38 +0700

    add: placement pick up button

--- src/components/ARPage/ARdisplay.jsx
+++ src/components/ARPage/ARdisplay.jsx
@@ -1,11 +1,11 @@
-import { ZapparCamera, ZapparCanvas, Loader, BrowserCompatibility,InstantTracker } from "@zappar/zappar-react-three-fiber";
+import { ZapparCamera, ZapparCanvas, Loader, BrowserCompatibility, InstantTracker } from "@zappar/zappar-react-three-fiber";
 import "./ar.css"
 import { useState } from "react";
 import * as THREE from 'three';
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 ); 
-const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} ); 
-const cube = new THREE.Mesh( geometry, material ); 
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
 
 const ARdisplay = () => {
   const [placementMode, setPlacementMode] = useState(false);
@@ -14,13 +14,26 @@ const ARdisplay = () => {
       <BrowserCompatibility />
       <div className="bg-red-200 w-full h-full min-h-screen flex flex-grow">
         <ZapparCanvas className="w-full min-h-screen bg-blue-200">
-          <ZapparCamera userCameraMirrorMode="poses"/>
+          <ZapparCamera userCameraMirrorMode="poses" />
           <InstantTracker placementMode={placementMode}>
-            <mesh geometry={geometry} material={material} position={[1, 1, -5]}/>
+            <mesh geometry={geometry} material={material} position={[1, 1, -5]} />
           </InstantTracker>
           <directionalLight position={[2.5, 8, 5]} intensity={1.5} />
           <Loader />
         </ZapparCanvas>
+        <div
+          className="absolute bottom-48 w-full"
+
+        >
+          <button className="mx-auto flex border-2 p-2 rounded-lg bg-slate-600 text-white"
+            onClick={() => {
+              setPlacementMode((currentPlacementMode) => !currentPlacementMode);
+            }}>
+            Tap here to
+            {placementMode ? " place " : " pick up "}
+            the object
+          </button>
+        </div>
       </div>
     </>
   )
