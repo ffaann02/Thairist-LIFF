589ffcabedd59882dbef192b8bf2de42eacaa248
Faan <rudfanno1@gmail.com>
2024-01-17 00:23:10 +0700

    fix: placement camera false

--- src/components/ARPage/ARdisplay.jsx
+++ src/components/ARPage/ARdisplay.jsx
@@ -8,15 +8,15 @@ const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
 const cube = new THREE.Mesh( geometry, material ); 
 
 const ARdisplay = () => {
-  const [placementMode, setPlacementMode] = useState(true);
+  const [placementMode, setPlacementMode] = useState(false);
   return (
     <>
       <BrowserCompatibility />
       <div className="bg-red-200 w-full h-full min-h-screen flex flex-grow">
         <ZapparCanvas className="w-full min-h-screen bg-blue-200">
           <ZapparCamera userCameraMirrorMode="poses"/>
-          <InstantTracker placementMode={placementMode} placementCameraOffset={[1, 1, -5]}>
-            <mesh geometry={geometry} material={material}/>
+          <InstantTracker placementMode={placementMode}>
+            <mesh geometry={geometry} material={material} position={[0, 0, -5]}/>
           </InstantTracker>
           <directionalLight position={[2.5, 8, 5]} intensity={1.5} />
           <Loader />
