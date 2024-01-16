fec654fec2bc1b113bbcafa455b8183cad05e2d9
Faan <rudfanno1@gmail.com>
2024-01-16 23:09:21 +0700

    add: camera AR

--- src/components/ARdisplay.jsx
+++ src/components/ARdisplay.jsx
@@ -1,9 +1,10 @@
-import { ZapparCamera, ZapparCanvas } from "@zappar/zappar-react-three-fiber";
+import { ZapparCamera, ZapparCanvas, Loader} from "@zappar/zappar-react-three-fiber";
 const ARdisplay = () => {
   return (
-    <div className="bg-red-200 w-full h-full min-h-screen">
-      <ZapparCanvas>
-        <ZapparCamera />
+    <div className="bg-red-200 w-full h-full min-h-screen flex flex-grow">
+      <ZapparCanvas className="w-full min-h-screen bg-blue-200">
+        <ZapparCamera rearCameraMirrorMode="css" />
+        <Loader />
       </ZapparCanvas>
     </div>
   )
