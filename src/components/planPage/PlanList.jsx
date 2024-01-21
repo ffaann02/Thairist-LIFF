4cd2f8c3392921384d3698e4cbcbfb2d6797236d
Chayapol Wongpuwarak <kongkcypw@gmail.com>
2024-01-21 23:25:20 +0700

    fix: re-name component

--- src/components/planPage/PlanList.jsx
+++ src/components/planPage/PlanList.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import { IoIosMore } from "react-icons/io";
-import AlertDelete from './alertDelete';
+import AlertDelete from './AlertDelete';
 
 const PlanList = ({ index, detail, refetch }) => {
 
