ac880655be6f8088e87ec0451025dcf4f1cd0ea6
Faan <rudfanno1@gmail.com>
2024-01-26 00:26:46 +0700

    add: image tracker

--- src/components/ARPage/AR.jsx
+++ src/components/ARPage/AR.jsx
@@ -1,11 +1,18 @@
-import { ZapparCanvas, ZapparCamera } from "@zappar/zappar-react-three-fiber";
-
+import { ZapparCanvas, ZapparCamera, ImageTracker } from "@zappar/zappar-react-three-fiber";
+import targetImage from "./example-tracking-image.zpt";
 const AR = () => {
   return (
     <ZapparCanvas className="absolute z-[1000]">
       <ZapparCamera onFirstFrame={() => {
-    console.log("first frame");
-  }}/>
+        console.log("first frame");
+      }} />
+      <ImageTracker targetImage={targetImage}
+        onVisible={(anchor) => {
+          console.log(`Visible ${anchor.id}`);
+          alert("hello");
+          }}>
+        <img src="https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg" />
+      </ImageTracker>
     </ZapparCanvas>
   )
 }
